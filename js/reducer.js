23480676772546b9afcaa772215e0038c92f8669
wickbushong <wickbushong@gmail.com>
2020-11-16 12:11:42 -0600

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -1,13 +1,25 @@
+
+let action = {type: 'INCREASE_COUNT'}
+let state = {count: 0};
+ 
 function changeState(state, action){
-  switch (action.type) {
-    case 'INCREASE_COUNT':
-      return {count: state.count + 1}
-    default:
-      return state;
+    switch (action.type) {
+      case 'INCREASE_COUNT':
+        return {count: state.count + 1}
+      default:
+        return state;
+    }
   }
+ 
+function render(){
+    document.body.textContent = state.count
+}
+ 
+function dispatch(action){
+  state = changeState(state, action)
+  render()
 }
 
-let state = {count: 0}
-let action = {type: 'INCREASE_COUNT'}
 
-changeState(state, action)
+
+
